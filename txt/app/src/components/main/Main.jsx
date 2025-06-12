f410fcfa73245851cb6c725abf41402b27ed085c
yasen <yasen50yasen50@gmail.com>
2025-06-12 19:53:03 +0300

    build boxes

---
+++ txt/app/src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './main.css'
+
+export default function Main() {
+  return (
+    <div className='main-div'>
+        <div className='box-1'>1</div>
+        <div className='box-2'>
+            <input type="text" />
+            <button>add</button>
+        </div>
+    </div>
+  )
+}
